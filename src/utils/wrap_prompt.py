8b20b3d744c4d54ad7b3de162a244965f3b1a7f7
matewolf <matewolf@cisco.com>
2024-12-06 13:06:10 +0100

    Update src/utils/wrap_prompt.py
    Co-authored-by: Tibor Kircsi <tkircsi@gmail.com>

--- src/utils/wrap_prompt.py
+++ src/utils/wrap_prompt.py
@@ -1,2 +1,8 @@
-def wrap_prompt(*args: str):
-    return "\n".join(args)
+def wrap_prompt(*args):
+    lines = []
+    for arg in args:
+        for line in arg.split("\n"):
+            # trim leading and trailing whitespace
+            line = line.strip()
+            lines.append(line)
+    return "\n".join(lines)
