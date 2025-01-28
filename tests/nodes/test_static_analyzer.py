78932eb6d5ef3b34dcdc6db6cf0038bbaecc4570
Zsolt Rappi <zrappi@cisco.com>
2025-01-28 12:46:20 +0100

    fix: unit tests

--- tests/nodes/test_static_analyzer.py
+++ tests/nodes/test_static_analyzer.py
@@ -1,3 +1,4 @@
+import os
 from subprocess import CompletedProcess
 from typing import Optional
 from unittest.mock import MagicMock, patch
@@ -8,26 +9,34 @@ from graphs.nodes.static_analyzer import StaticAnalyzer
 
 from graphs.states import GitHubPRState, create_default_github_pr_state
 from tests.nodes.utils import vector_based_similarity
+from utils.constants import AZURE_OPENAI_API_KEY_ENV
 from utils.github_operations import GitHubOperations
 from utils.modelfactory import models
 
-mock_output_folder = "./repo_copy/test-repo"
+_mock_output_folder = "./repo_copy/test-repo"
 
-expected_summary = """\
+_expected_summary = """\
 1. main.tf: "10.0.2.0.12/24" is not a valid CIDR block: invalid CIDR address: 10.0.2.0.12/24 with aws_subnet.private_subnet, on main.tf, in resource "aws_subnet" "private_subnet": cidr_block = "10.0.2.0.12/24"
 2. main.tf: Reference to undeclared resource on main.tf, in resource "aws_route_table_association" "public_rt_assoc": route_table_id = aws_route_table.public_rt.id. A managed resource "aws_route_table" "public_rt" has not been declared in the root module.
 3. main.tf: Reference to undeclared resource on main.tf, in resource "aws_instance" "web_server": security_groups = [aws_security_group.main_sg.name]. A managed resource "aws_security_group" "main_sg" has not been declared in the root module.
 """
 
+_tf_validate_args = ["terraform", "validate", "-no-color"]
+_tflint_args = ["tflint", "--format=compact", "--recursive"]
+
 
 @pytest.fixture
 def mock_context() -> DefaultContext:
-    model = models.get_azure_openai()
+    # In other tests the secret manager is mocked and set to None by default, so we have to patch it here with a real SM instance
+    with patch("utils.modelfactory.secret_manager") as mock_secret_manager:
+        mock_secret_manager.azure_openai_api_key = os.getenv(AZURE_OPENAI_API_KEY_ENV)
+        model = models.get_azure_openai()
+
     context = DefaultContext()
     context.chain = create_static_analyzer_chain(model)
 
     github = MagicMock(spec=GitHubOperations)
-    github.clone_repo.return_value = mock_output_folder
+    github.clone_repo.return_value = _mock_output_folder
 
     context.github = github
 
@@ -42,9 +51,9 @@ def mock_state() -> GitHubPRState:
 
 def mock_run_logic(args, **kwargs) -> Optional[CompletedProcess]:
     match args:
-        case ["terraform", "validate", "-no-color"]:
+        case x if x == _tf_validate_args:
             return CompletedProcess(
-                args=["terraform", "validate", "-no-color"],
+                args=_tf_validate_args,
                 returncode=1,
                 stdout="",
                 stderr="""
@@ -64,9 +73,9 @@ def mock_run_logic(args, **kwargs) -> Optional[CompletedProcess]:
                 the root module.
                 """,
             )
-        case ["tflint", "--format=compact", "--recursive"]:
+        case x if x == _tflint_args:
             return CompletedProcess(
-                args=["tflint", "--format=compact", "--recursive"],
+                args=_tflint_args,
                 returncode=2,
                 stdout="""4 issue(s) found:
                 main.tf:53:1: Warning - Missing version constraint for provider "aws" in `required_providers` (terraform_required_providers)
@@ -109,7 +118,9 @@ def test_static_analyzer_call(mock_run, mock_rmtree, mock_context, mock_state):
     cf = StaticAnalyzer(mock_context)
     resp = cf(mock_state)
 
-    assert mock_rmtree.one_called_with(mock_output_folder)
+    mock_run.assert_any_call(_tf_validate_args, cwd=_mock_output_folder, capture_output=True, text=True)
+    mock_run.assert_any_call(_tflint_args, cwd=_mock_output_folder, capture_output=True, text=True)
+    mock_rmtree.assert_called_with(_mock_output_folder)
+
     summary = resp["static_analyzer_output"]
-    assert summary
-    assert vector_based_similarity(summary, expected_summary) > 0.95
+    assert vector_based_similarity(summary, _expected_summary) > 0.95
