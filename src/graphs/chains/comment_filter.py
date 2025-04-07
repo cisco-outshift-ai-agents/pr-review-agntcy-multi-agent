afdd428efba28b50c2463bf35482cc1db0391ac7
SreeGowriAddepalli <sreeadde@cisco.com>
2025-04-07 13:02:13 -0400

    Update comment_filter

--- src/graphs/chains/comment_filter.py
+++ src/graphs/chains/comment_filter.py
@@ -6,41 +6,34 @@ from utils.wrap_prompt import wrap_prompt
 from utils.models import ReviewComments
 from typing import cast
 
+from langchain.schema import SystemMessage, HumanMessage
+
 
 def create_comment_filter_chain(model: BaseChatModel) -> RunnableSerializable[dict, dict | ReviewComments]:
+
     llm_with_structured_output = cast(RunnableSerializable[dict, dict | ReviewComments], model.with_structured_output(ReviewComments))
-    system_message = wrap_prompt("""\
-        You are a review agent tasked with filtering a list of PR review comments.
-        Your peer has created several comments on a GitHub pull request but it could be that some of them are not useful.
-        Your job is to filter these comments based on the instructions below, follow them carefully.
 
-        Input Format:
-        comments: The set of comments that you need to filter.
-        Here's an example how the input array will look like:
-        {input_json_format}
+    system_message = SystemMessage(content=" You are a review agent tasked with filtering a list of PR review comments.\
+                                   Your peer has created several comments on a GitHub pull request but it could be that some of them are not useful.\
+                                   Your job is to filter these comments based on the instructions below, follow them carefully. \
+                                   \
+                                   Rules for Filtering: \
+                                   A comment is considered not useful if ANY of the following applies: \
+                                   - It just states a simple statement or positive feedback or what the user changed in the code without stating a clear issue. \
+                                   - It doesn't mention any actionable item. \
+                                   \
+                                   Input Format: \
+                                   comments: The set of comments that you need to filter. \
+                                   {input_json_format} \
+                                   \
+                                   Response format: \
+                                   Return ONLY the remaining comments without changing the content or format or anything else in them. \
+                                   DO NOT USE markdown in the response.")
 
-        Rules for Filtering:
-        A comment is considered not useful if ANY of the following applies:
-        - It just states a simple statement or positive feedback or what the user changed in the code without stating a clear issue.
-        - It doesn't mention any actionable item.
-
-        Response format:
-        Return ONLY the remaining comments without changing the content or format or anything else in them.
-        DO NOT USE markdown in the response.
-        """)
+    user_message = HumanMessage(content="{question}")
 
     prompt = ChatPromptTemplate.from_messages(
-        messages=[
-            (
-                "system",
-                system_message,
-            ),
-            ("user", "{question}"),
-        ],
+        messages=[system_message, user_message]
     )
 
     return prompt | llm_with_structured_output
-
-
-## Where is it written as list? of PR review comments.
-## input array  or list??
